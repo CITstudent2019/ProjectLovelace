2a568511b4431902d03eba03bf22cbdf54154421
SnoopLaserSnake <38845982+SnoopLaserSnake@users.noreply.github.com>
2019-01-21 22:52:54 -0600

    Update chaos.py

--- chaos.py
+++ chaos.py
@@ -2,7 +2,10 @@
 # date: 1-21-2019
 # desc: choas, calculates a logistic map using formula Xn+1 = rXn(1 - Xn)
 
+# 0 < r < 4
 def logistic_map(r):
+	# each value in x is the population size each year
+	# 1 is 100% of the maximum possible population, .5 is 50%, etc.
 	x = [0.5]
 	
 	while len(x) <= 50:
